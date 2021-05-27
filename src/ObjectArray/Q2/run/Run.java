c2b926f5ee8b5fa9be85bd6c7971b96b3c6ee55f
jaewoo-k <72554044+jaewoo-k@users.noreply.github.com>
2021-05-27 18:54:24 +0900

    Add files via upload

---
+++ src/ObjectArray/Q2/run/Run.java
@@ -0,0 +1,10 @@
+package ObjectArray.Q2.run;
+
+import ObjectArray.Q2.controller.ProductController;
+
+public class Run {
+	public static void main(String[] args) {
+//		ProductController pc = new ProductController();
+		new ProductController().mainMenu();
+	}
+}
