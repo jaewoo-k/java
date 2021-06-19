84e2ce45c0563faf73de29113fe75ac6b019c00b
jaewoo-k <jwko0704@gmail.com>
2021-06-19 10:53:02 +0900

    review

--- src/baekjoon/forLoop/LessThenX.java
+++ src/baekjoon/forLoop/LessThenX.java
@@ -1,5 +1,11 @@
 package baekjoon.forLoop;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class LessThenX {
 /**
 X보다 작은 수
@@ -15,9 +21,60 @@ X보다 작은 수
 출력
 X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
  * @param args
+ * @throws IOException 
  */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		
+		// N : 배열의 크기 (안의 숫자는 1~10000)
+		// X : X보다 작은 수 (1~10000)
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < N; i++) {
+			int value = Integer.parseInt((st.nextToken()));
+			if(value < X) {
+				sb.append(value + " ");
+			}
+		}
+		System.out.println(sb);
+		/*origin
+		int[] arr = new int[N];
+		int count = 0;
+//		int[] arr2 = new int[N];
+		
+		// 다음 한줄 읽어와서 배열에 담아주기
+		st = new StringTokenizer(br.readLine(), " ");
+		while(st.hasMoreTokens()) {
+			arr[count++] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 배열과 X를 비교해서 count++
+		count = 0;
+		for(int i = 0; i < N; i++){
+			if(arr[i] < X) {
+				count++;
+			}
+		}
+		
+//		int[] arr2 = new int[count];
+		// 배열2 생성하지말고 바로 sb에 넣고 출력
+		for(int i = 0; i < N; i++){
+			if(arr[i] < X) {
+				sb.append(arr[i] + " ");
+			}
+		}
+		System.out.println(sb);
+		 */
+		
+		
+		
+		
+		
 		
 	}
-
 }
